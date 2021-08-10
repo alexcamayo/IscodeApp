f86b6a8bcf8dd2dad713aeba315e728b97278bf0
Alex Camayo <80357104+alexcamayo@users.noreply.github.com>
2021-08-10 08:26:00 -0500

    Update ApiService.java

--- app/src/main/java/com/proyecto/iscodeapp/Notificaciones/ApiService.java
+++ app/src/main/java/com/proyecto/iscodeapp/Notificaciones/ApiService.java
@@ -7,7 +7,7 @@ import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface ApiService {
-    @Headers({"Authorization: key=AAAAOnl2yHQ:APA91bFo6KzPhcOCZh8jnsqahacf0jocBGCRlefIk90bp12RDbk7yuAmNh7XNHt0-3WMZVcT874PVhbTGzzMsNZdCrASVyv0TQ5CVnf3U2DrmYItAhPaLBiYQcsFYUhK85cvE-bNhRBI",
+    @Headers({"Authorization: key=Key de Firebase Messaging va aquí",
             "Content-Type:application/json"})
     @POST("fcm/send")
     Call<ResponseBody> sendChatNotification(@Body RequestNotificaton requestNotificaton);
